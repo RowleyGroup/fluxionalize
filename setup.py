248a3c66914b732cd3c46dc9e10429799965c971
kgaalswyk <kr.gaalswyk@gmail.com>
2015-08-12 15:48:43 -0230

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,8 +31,11 @@ def write_sh():
                                 file_ext='.SMILES'
 
         if name_spec==False:
-                name = file_name
-        file_out=open('test3.sh', 'w')
+                if file_ext=='.SMILES':
+                        name=test
+                else:
+                        name = file_name
+        file_out=open(name+'.sh', 'w')
         file_out.write('#PBS -l walltime=120:00:00\n#PBS -l nodes='+num_procs+'\n#PBS -N '+name+'\n#PBS -o '+name+'.out\n#PBS -j oe\n\ncd '+location+'\n\n'
                         +'module load openbabel\nmodule load intel64/14.0.0.080\nmodule load openmpi_intel64/1.6.5_intel14\nmodule load amber\nmodule load vmd64\nmodule load namd/2.10\n\n'
                         +'python tes.py -n $[PBS_NUM_NODES*24] -c '+num_clusters+' -i "'+file_ext[1:]+' '+input+' '+name+'"\n')
